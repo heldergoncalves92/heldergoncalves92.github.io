d77668b492c8ad6a633940303427e4b42b887d8d
Hélder Gonçalves <helder.goncalves@outsystems.com>
2021-03-28 16:08:24 +0100

    Add basic router

---
+++ src/About/About.tsx
@@ -0,0 +1,10 @@
+
+const About = () => {
+    return (
+        <div>
+          <h2>Home</h2>
+        </div>
+    );
+}
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Home from "./Home/Home";
+import Contact from "./Contact/Contact";
+import About from "./About/About";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+export enum Routes {
+  Home = "/",
+  About = "/about",
+  Contact = "/contact",
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+
+        <h2>Welcome to React Router Tutorial</h2>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={Routes.Home} className="nav-link"> Home </Link></li>
+            <li><Link to={Routes.Contact} className="nav-link">Contact</Link></li>
+            <li><Link to={Routes.About} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path={Routes.Home} component={Home} />
+              <Route path={Routes.Contact} component={Contact} />
+              <Route path={Routes.About} component={About} />
+          </Switch>
+      </Router>
   );
 }
 
